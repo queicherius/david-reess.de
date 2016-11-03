5a8ff6e6d0f4d433a4ae12332cf924688437fa53
queicherius <queicherius@gmx.de>
2016-11-03 04:36:23 +0100

    Import from Mai 2014

---
+++ site/templates/error.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html>
+
+	<head>
+		<title>Page not found</title>
+
+		<style>
+
+			* { 
+				padding:0; 
+				margin:0;
+			}
+
+			html, body{
+				height: 100%;
+				width: 100%;
+				overflow: hidden;
+				background: #66ccff;
+			}
+
+			.image{
+				background: #66ccff url('/assets/images/calm-cat.gif') no-repeat center center;
+				min-width: 150px;
+				min-height: 170px;
+			}
+
+			h1, h2, h3 {
+				font-family: Arial;
+				font-size: 30px;
+				color: white;
+				text-align: center;				
+			}
+
+			h1 {
+				font-size: 70px;
+				opacity: 0.4;
+			}
+
+			.middle {
+				height: 290px;
+				width: 100%;
+				margin-top: -145px;
+				position: absolute;
+				top: 50%;
+			}
+
+		</style>
+
+	</head>
+
+	<body>
+
+		<div class="middle">
+
+			<div class="image"></div>
+			<h2>There is nothing here, sorry</h2>
+			<h1>404</h1>
+
+		</div>
+
+	</body>
+
+</html>
